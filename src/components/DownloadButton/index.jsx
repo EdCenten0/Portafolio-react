529ad659d7df18a0199e67a0dd09c7215c381dc5
Ed Centeno <cchavarriacenteno8@gmail.com>
2024-01-08 17:03:26 -0600

    Download button is properly coded as a react component

--- src/components/DownloadButton/index.jsx
+++ src/components/DownloadButton/index.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
-const DownloadButton = () => {
+const DownloadButton = ({ downloadURL, buttonTitle }) => {
   return (
-    <a
-      href="https://firebasestorage.googleapis.com/v0/b/portafolio-react-27e84.appspot.com/o/Carlos%20Eduardo%20Chavarr%C3%ADa%20CV.pdf?alt=media&token=36056c48-dccb-4cea-9c5d-d159b4dc4095"
-      download
-      rel="noreferrer noopener"
-      target="_blank"
-    >
+    <a href={downloadURL} download rel="noreferrer noopener" target="_blank">
       <button
         className=" p-3 rounded-lg bg-slate-700 text-white mt-2 hover:bg-slate-600 active:bg-slate-200 transition-all "
         onClick={() => {}}
       >
-        Curriculum Vitae
+        {buttonTitle}
       </button>
     </a>
   );
